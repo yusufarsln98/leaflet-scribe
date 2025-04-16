13cf28df26ad530f627aed73b449837e944324e4
Yusuf Arslan <yusufarsln98@outlook.com>
2025-04-16 16:19:22 +0300

    fix: drawing rectangle and editing circle issues (Uncaught ReferenceError)

--- build/publish.sh
+++ build/publish.sh
@@ -11,18 +11,6 @@ git checkout -b build
 jake build[,,true]
 jake docs
 
-git add \
-    dist/leaflet.draw.js \
-    dist/leaflet.draw-src.js \
-    dist/leaflet.draw-src.map \
-    dist/leaflet.draw.css \
-    dist/leaflet.draw-src.css \
-    docs/* \
-		index.d.ts `
-    -f
-
-git commit -m "v$VERSION"
-
 git tag v$VERSION -f
 git push --tags -f
 
